df66043716f1c224a52b137c6a108cddbf70c11f
inesfproenca <inesfproenca@gmail.com>
2016-03-02 12:03:09 +0000

    Project first upload

---
+++ Jogo do labirinto/src/maze/logic/TestMaze.java
@@ -0,0 +1,25 @@
+package maze.logic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMaze {
+
+	@Test
+	public void testMaze() {
+		//Maze maze = new Maze(1);
+		//assertEquals(0, maze.update('e'));
+	}
+	
+	/* Se teste lançar exceção usar
+	 * 
+	 * @Test(expected = IllegalArgumentException.class)
+	 * public void testGcdZero{
+	 * 		MyMath.gcd(0,2);
+	 * }
+	 * 
+	 * Para lançamento de exceções -> 1 teste por exceção
+	 */
+
+}
